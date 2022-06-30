92a2d2df5c2edb05030888f3e134eb28776c7c07
Shuvapandit <chakrabortyshuva216@gmail.com>
2022-06-30 20:29:08 +0600

    commit navbar

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        doctorsportaltheme: {
+          primary: "#442B73",
+          secondary: "#19D3AE",
+          accent: "#3A4256",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      
+    ],
+  },
+  plugins: [require("daisyui")],
+}
